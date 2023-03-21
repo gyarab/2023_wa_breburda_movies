8725eb02002860042ec7f3ab4efdb5d2d9ce5749
SklenarMiroslav <miroslav.sklenar@student.gyarab.cz>
2023-03-21 09:24:17 +0100

    UwU

--- gamdb/movies/admin.py
+++ gamdb/movies/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Movies
+from .models import Movies, Director, Genres
 
 class MoviesAdmin(admin.ModelAdmin):
-    list_display = ['id','name', 'year']
+    list_display = ['id','name', 'year', 'director', 'genres_display']
     list_display_links = ['id','name']
     search_fields = ['name']
+    list_filter = ['genres', 'year']
+
+
 
 admin.site.register(Movies, MoviesAdmin)
+admin.site.register(Director)
+admin.site.register(Genres)
 
 # Register your models here.
--- gamdb/movies/models.py
+++ gamdb/movies/models.py
@@ -1,9 +1,33 @@
 from django.db import models
 class Movies(models.Model):
     name = models.CharField(max_length=200)
-    year = models.IntegerField()
+    year = models.IntegerField(blank=True, null=True)
     description = models.TextField()
+    director = models.ForeignKey('Director', on_delete=models.SET_NULL, blank=True, null=True)
+    genres = models.ManyToManyField('Genres')
 
     def __str__(self):
         return f"{self.name} ({self.year})"
+
+    def genres_display(self):
+        #self.genres.all()
+        #out = ""
+        #for genre in self.genres.all():
+        #    out += f"{genre.name}, "
+        #return out
+        return ', '.join(genre.name for genre in self.genres.all()[:3])
+
+class Director(models.Model):
+    name = models.CharField(max_length=200)
+    birth_year = models.IntegerField(blank=True, null=True)
+    #movies = models.ManyToManyField(Movies)
+
+    def __str__(self):
+        return f"{self.name} ({self.birth_year})"
+
+class Genres(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
 # Create your models here.
