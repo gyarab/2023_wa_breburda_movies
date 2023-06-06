e707a05b1406b75fa4b2115f51a14ba35d59c2f2
SklenarMiroslav <miroslav.sklenar@student.gyarab.cz>
2023-06-06 08:30:03 +0200

    uwu

--- gamdb/gamdb/settings.py
+++ gamdb/gamdb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b=%la^of)jcypio90tdt1gv*e3x@)=fhf%=-v5(78v#inwf%m-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
